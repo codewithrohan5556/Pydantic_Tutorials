1baa573918d0a0f8289166526ed8e55cac8a605c
Rohan Patil <166194152+codewithrohan5556@users.noreply.github.com>
2026-05-04 15:27:35 +0530

    Add files via upload

---
+++ 6_computed_feilds.py
@@ -0,0 +1,46 @@
+from pydantic import BaseModel, EmailStr, computed_field
+from typing import List, Dict
+
+
+class Patient(BaseModel):
+
+    name: str
+    email : EmailStr
+    age: int
+    weight: float
+    height: float
+    married: bool
+    allergies: List[str]  
+    contact_details: Dict[str, str]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = round(self.weight / (self.height**2),2)
+        return bmi
+    
+
+                
+
+def update_patient_data(patient: Patient):
+    print(patient.name)
+    print(patient.age)
+    print(patient.allergies)
+    print(patient.married)
+    print(patient.bmi)
+    print('updated')
+
+
+patient_info = {
+    'name': 'nitish',
+    'email': 'abc@hdfc.com',
+    'age': '70',
+    'weight': 65.5,
+    'height':1.72,
+    'married': True,        
+    'allergies': ['dust'],  
+    'contact_details': { 'phone': '1234567890', 'emergency': '9876543210'}
+}
+
+patient1 = Patient(**patient_info)  # validation -> type coercion
+update_patient_data(patient1)
